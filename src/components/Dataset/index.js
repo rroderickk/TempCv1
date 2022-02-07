01aa5e65ed13b95a67e462b0b3b198952097f106
RodrigoMoreno <rroderickk@gmail.com>
2022-02-07 12:27:31 -0500

    [ADD] dataset

--- src/components/Dataset/index.js
+++ src/components/Dataset/index.js
@@ -1,21 +1,70 @@
-import reacjs     from "./react.png";      import node from "./node.png";
-import javascript from "./javascript.png"; import nest from "./nestjs.png";
+import reacjs from "./react.png";                        import node from "./node.png";
+import javascript from "./javascript.png";               import nest from "./nestjs.png";
 import pythonProfesional from "./pythonProfesional.png"; import sololearn from "./SoloLearnJavascript.png";
-import closures   from "./closures.png";   import poo from "./objetos.png";
-import tiempo from "./tiempo.png";         import py from "./python.png";
+import closures from "./closures.png";                   import poo from "./objetos.png";
+import tiempo from "./tiempo.png";                       import py from "./python.png";
 
 const dataset = 
-{ assets:
-[ 
-{ 0: 0, id:"Desarrollar una tienda online con React Llevar un diseño de HTML y CSS a React Crear tu primer sitio web con React.js", name:"ReactJs",     token_id:"0", traits:[0],  image_original_url: reacjs    , urlPlatzi:"https://raw.githubusercontent.com/rroderickk/TempCv1/gh-pages/src/components/Dataset/react.png"},
-{ 1: 1, id:"Conocer los conceptos básicos de Node.js, Cómo manejar la asincronía, Entender los módulos del core, Utilizar los módulos y paquetes externos", name:"NodeJs",      token_id:"1", traits:[1],  image_original_url: node      , urlPlatzi: "https://platzi.com/p/rroderickk/curso/1759-fundamentos-node/diploma/detalle/" },
-{ 2: 2, id:"Despliega tus proyectos a internet con GitHub Pages. Implementa un flujo de desarrollo web profesional. Conecta JavaScript con HTML.", name:"Javascript",  token_id:"2", traits:[2],  image_original_url: javascript, urlPlatzi:"" }, 
-{ 3: 3, id:"Estructura y arquitectura de un proyecto backend Programa controladores, servicios y pipes personalizados Construye el CRUD de una API REST", name:"NestJs",      token_id:"3", traits:[3],  image_original_url: nest      , urlPlatzi:"" },
-{ 4: 4, id:4, name:"Python Profesional", token_id:"4", traits:[4], image_original_url: pythonProfesional , urlPlatzi:"" },
-{ 5: 5, id:5, name:"Closures",    token_id:"5", traits:[5],  image_original_url: closures  , urlPlatzi:"" },
-{ 6: 6, id:6, name:"JsSoloLearn", token_id:"6", traits:[6],  image_original_url: sololearn , urlPlatzi:""},
-{ 7: 7, id:7, name:"Poo",         token_id:"7", traits:[7],  image_original_url: poo  , urlPlatzi:""},
-{ 8: 8, id:9, name:"Gestion efectiva del tiempo", token_id:"8", traits:[9],  image_original_url: tiempo, urlPlatzi:""  },
-{ 9: 9, id:9, name:"Python",    token_id:"9", traits:[9],  image_original_url: py , urlPlatzi:""},
-]
-}; export default  dataset;
+{ assets: 
+[{
+    0: 0,
+    id: "Desarrollar una tienda online con React Llevar un diseño de HTML y CSS a React Crear tu primer sitio web con React.js",
+    name: "ReactJs", token_id: "0", traits: [0], image_original_url: reacjs,
+    urlPlatzi: "https://raw.githubusercontent.com/rroderickk/TempCv1/gh-pages/src/components/Dataset/react.png",
+  },
+  {
+    1: 1,
+    id: "Conocer los conceptos básicos de Node.js, Cómo manejar la asincronía, Entender los módulos del core, Utilizar los módulos y paquetes externos",
+    name: "NodeJs", token_id: "1", traits: [1], image_original_url: node,
+    urlPlatzi: "https://platzi.com/p/rroderickk/curso/1759-fundamentos-node/diploma/detalle/",
+  },
+  {
+    2: 2,
+    id: "Despliega tus proyectos a internet con GitHub Pages. Implementa un flujo de desarrollo web profesional. Conecta JavaScript con HTML.",
+    name: "Javascript", token_id: "2", traits: [2], image_original_url: javascript,
+    urlPlatzi: "",
+  },
+  {
+    3: 3,
+    id: "Estructura y arquitectura de un proyecto backend Programa controladores, servicios y pipes personalizados Construye el CRUD de una API REST",
+    name: "NestJs", token_id: "3", traits: [3], image_original_url: nest,
+    urlPlatzi: "",
+  },
+  {
+    4: 4,
+    id: "Manejarás iteradores y generadores Crearás decoradores Entenderás qué son los closures y scopes Aplicarás tipado estático en Python",
+    name: "Python Profesional", token_id: "4", traits: [4], image_original_url: pythonProfesional,
+    urlPlatzi: "",
+  },
+  {
+    5: 5,
+    id: "Describir el concepto de Scope en JS Identificar el uso de Local Scope, Function Scope y Block Scope Describir el concepto de Closure en JavaScript Comprender el concepto de Ámbito Léxico",
+    name: "Closures", token_id: "5", traits: [5], image_original_url: closures,
+    urlPlatzi: "",
+  },
+  {
+    6: 6,
+    id: "Learn all the basic features of JavaScript, including making your website more interactive, changing website content, validating forms, and so much more.",
+    name: "JsSoloLearn", token_id: "6", traits: [6], image_original_url: sololearn,
+    urlPlatzi: "",
+  },
+  {
+    7: 7,
+    id: "Comprende cómo funcionan los objetos en JavaScript. Descubre las ventajas de utilizar POO.  Ordena tu código con abstracción y encapsulamiento.  Reutiliza tu código con herencia y polimorfismo.",
+    name: "Poo", token_id: "7", traits: [7], image_original_url: poo,
+    urlPlatzi: "",
+  },
+  {
+    8: 8,
+    id: "Conocer conceptos de la gestión del tiempo Implementar técnicas de gestión del tiempo Profundizar en estrategias de gestión del tiempo Priorizar tareas",
+    name: "Gestion efectiva del tiempo", token_id: "8", traits: [9], image_original_url: tiempo,
+    urlPlatzi: "",
+  },
+  {
+    9: 9,
+    id: "Hacer estructuras de datos Crear bucles Conocer herramientas para programar Aprender conceptos básicos de Python",
+    name: "Python", token_id: "9", traits: [9], image_original_url: py,
+    urlPlatzi: "",
+  },
+],}; export default dataset;
+
