f313dd3d89898fd4ac8bd3755855edfb41014e07
RodrigoMoreno <rroderickk@gmail.com>
2022-02-07 12:32:22 -0500

    [ADD] complete dataset

--- src/components/Dataset/index.js
+++ src/components/Dataset/index.js
@@ -6,7 +6,7 @@ import tiempo from "./tiempo.png";                       import py from "./pytho
 
 const dataset = 
 { assets: 
-[{
+  [{
     0: 0,
     id: "Desarrollar una tienda online con React Llevar un diseño de HTML y CSS a React Crear tu primer sitio web con React.js",
     name: "ReactJs", token_id: "0", traits: [0], image_original_url: reacjs,
@@ -22,49 +22,49 @@ const dataset =
     2: 2,
     id: "Despliega tus proyectos a internet con GitHub Pages. Implementa un flujo de desarrollo web profesional. Conecta JavaScript con HTML.",
     name: "Javascript", token_id: "2", traits: [2], image_original_url: javascript,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/javascript.png?raw=true",
   },
   {
     3: 3,
     id: "Estructura y arquitectura de un proyecto backend Programa controladores, servicios y pipes personalizados Construye el CRUD de una API REST",
     name: "NestJs", token_id: "3", traits: [3], image_original_url: nest,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/nestjs.png?raw=true",
   },
   {
     4: 4,
     id: "Manejarás iteradores y generadores Crearás decoradores Entenderás qué son los closures y scopes Aplicarás tipado estático en Python",
     name: "Python Profesional", token_id: "4", traits: [4], image_original_url: pythonProfesional,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/python.png?raw=true",
   },
   {
     5: 5,
     id: "Describir el concepto de Scope en JS Identificar el uso de Local Scope, Function Scope y Block Scope Describir el concepto de Closure en JavaScript Comprender el concepto de Ámbito Léxico",
     name: "Closures", token_id: "5", traits: [5], image_original_url: closures,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/closures.png?raw=true",
   },
   {
     6: 6,
     id: "Learn all the basic features of JavaScript, including making your website more interactive, changing website content, validating forms, and so much more.",
     name: "JsSoloLearn", token_id: "6", traits: [6], image_original_url: sololearn,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/objetos.png?raw=true",
   },
   {
     7: 7,
     id: "Comprende cómo funcionan los objetos en JavaScript. Descubre las ventajas de utilizar POO.  Ordena tu código con abstracción y encapsulamiento.  Reutiliza tu código con herencia y polimorfismo.",
     name: "Poo", token_id: "7", traits: [7], image_original_url: poo,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/objetos.png?raw=true",
   },
   {
     8: 8,
     id: "Conocer conceptos de la gestión del tiempo Implementar técnicas de gestión del tiempo Profundizar en estrategias de gestión del tiempo Priorizar tareas",
     name: "Gestion efectiva del tiempo", token_id: "8", traits: [9], image_original_url: tiempo,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/tiempo.png?raw=true",
   },
   {
     9: 9,
     id: "Hacer estructuras de datos Crear bucles Conocer herramientas para programar Aprender conceptos básicos de Python",
     name: "Python", token_id: "9", traits: [9], image_original_url: py,
-    urlPlatzi: "",
+    urlPlatzi: "https://github.com/rroderickk/TempCv1/blob/gh-pages/src/components/Dataset/python.png?raw=true",
   },
 ],}; export default dataset;
 
