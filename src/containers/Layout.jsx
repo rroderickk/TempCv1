fe6d77844c52c9656176107264050d51659c7133
rroderickk <rroderickk@gmail.com>
2022-03-04 22:31:06 -0500

    [ADD] fix layout

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import "./Layout.scss";
 
-const Layout =({ children })=> <div className="Layout">{children}</div> 
+const Layout =({ children })=> <body className="Layout">{children}</body> 
 
 export default Layout;
