2ac345167adb6f4fee001a87a509abe069201f82
rroderickk <rroderickk@gmail.com>
2022-03-05 13:05:27 -0500

    [ADD] vscodetheme projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -16,8 +16,8 @@ const Projects =()=> { return ( <>
 <a className="a--container" href="https://github.com/rroderickk/nodemachine" target="_blank" rel="noreferrer noopener">
   <span className="a--nodemachine"></span> 
 </a>
-{/* <a className="a--container" href="https://github.com/rroderickk/matrixResolutions" target="_blank" rel="noreferrer noopener">
-  <span className="a--mresolutions" /> 
-</a> */}
+<a className="a--container" href="https://github.com/rroderickk/matrixResolutions" target="_blank" rel="noreferrer noopener">
+  <div className="mcontenedor"><span className="a--mresolutions" /></div>
+</a>
 </footer>
 </> ) }; export default Projects;
