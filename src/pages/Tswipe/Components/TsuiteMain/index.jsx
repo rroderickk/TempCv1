a73bfadadedb34f3d4849cff88ceb3a470d3c354
rroderickk <rroderickk@gmail.com>
2022-03-09 20:04:32 -0500

    [FIX] styles main

--- src/pages/Tswipe/Components/TsuiteMain/index.jsx
+++ src/pages/Tswipe/Components/TsuiteMain/index.jsx
@@ -6,7 +6,7 @@ const TsuiteMain =()=> { return ( <>
 <div className="tsuite-main--container textRelativo">
   <div className="tsuite-carrousel--container ">
     <div className="tsuite--carrousel">
-      <h1 className="h1 textRelativo">Con nuestros algoritmos de inteligencia artificial maximizas el rendimiento de reservas en viviendas vacacionales, apartamentos turisticos y establecimientos hoteleros.</h1>
+      <h1 className="h1 textRelativo">Con nuestros algoritmos de inteligencia artificial maximizas el rendimiento de tus reservas en viviendas vacacionales, apartamentos turisticos y establecimientos hoteleros.</h1>
     </div>
   </div>
 </div>
