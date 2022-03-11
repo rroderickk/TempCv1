7bcc54742321be8baf508572e1ad5e994e3f5488
rroderickk <rroderickk@gmail.com>
2022-03-11 03:15:10 -0500

    [ADD] fancyButtons mold, tsuiteBody

--- src/pages/Tswipe/Components/FancyButton/index.jsx
+++ src/pages/Tswipe/Components/FancyButton/index.jsx
@@ -9,17 +9,22 @@ const button ={
   fontFamily: `${!props.font? "Roboto":props.font}`,
   fontSize:     `${!props.fsize? "11.2px":props.fsize}`,
   width:         `${!props.width? "200px":props.width}`,
-  padding:       `${!props.padding? "1.2rem 2rem":props.padding}`,
-  color:         `${!props.color? "white":props.color}`,
+  padding:        `${!props.padding? "1.2rem 2rem":props.padding}`,
   backgroundColor:`${!props.bg? "transparent":props.bg}`,
-  letterSpacing: `${!props.spa? "2.2px":props.spa}`,
+  letterSpacing:  `${!props.spa? "2.2px":props.spa}`,
   border:        `1px solid ${!props.bor? "white":props.bor}`,
+  color:        `${!props.color? "white":props.color}`,
 };
 
 return (  <>
 <div style={container} className="fancy-button--container">
   <span className="fancy-span"></span>
-  <a style={button} className="fancy-button--a" 
+  <a style={button} 
+  className=
+  {
+    `fancy-button--a ${props.c1 && "fancy-red"} || ${props.c2 && "fancy-salmon"}
+    || ${props.c3 && "fancy-orange"} || ${props.c4 && "fancy-crims"}
+  `} 
     href={props.to} rel="noreferrer noopener" target="_blank">{props.text}</a>
 </div>
 </> ) }; export default FancyButton;
---
+++ src/pages/Tswipe/Components/TsuiteBody/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.scss";
+
+const TsuiteBody =()=> { return ( <>
+
+<div className="tsuite-body--container">TsuiteBody</div>
+
+</> ) }; export default TsuiteBody;
