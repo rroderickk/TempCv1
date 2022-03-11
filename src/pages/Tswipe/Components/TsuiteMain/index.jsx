54b4cc1eaf5d28ced430a70d91285f8be1671785
rroderickk <rroderickk@gmail.com>
2022-03-10 23:23:35 -0500

    [FIX] many styles

--- src/pages/Tswipe/Components/TsuiteMain/index.jsx
+++ src/pages/Tswipe/Components/TsuiteMain/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Awesome from "../Awesome";
-
 import "./index.scss";
+const handleScroll =(param)=>()=> window.scrollTo(0, param);
+
+function newwindow(){  
+  var myChild= window.open('https://turbosuite.es/index.php','','width=1024,height=768,resizable=no');  
+  myChild.blur();
+};
 
 const TsuiteMain =()=> { return ( <>
 <Awesome/>
@@ -11,10 +16,11 @@ const TsuiteMain =()=> { return ( <>
     <div className="tsuite--carrousel">
       <h1 className="h1 textRelativo">Con nuestros algoritmos de inteligencia artificial maximizas el rendimiento de tus reservas en viviendas vacacionales, apartamentos turisticos y establecimientos hoteleros.</h1>
       <div className="learnmore--container">
-        <a href="https://turbosuite.es/index.php" className="b--learnmore" target="_blank"  rel="noopener noreferrer">APRENDE MAS +</a>
+        <a onClick={()=>newwindow()} className="b--learnmore">APRENDE MAS +</a>
       </div>
     </div>
   </div>
+<div className="i--container" onClick={handleScroll(1000)} rel="noreferrer noopener"><i className="i--flechita">►</i></div>
 </div>
 </main>
 </> ) }; export default TsuiteMain;
