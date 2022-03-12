b9131fb71b68c8b6188affb100f0d55f6649d29c
rroderickk <rroderickk@gmail.com>
2022-03-12 00:06:03 -0500

    [ADD] demo main

--- src/pages/Tswipe/Components/Banner/index.jsx
+++ src/pages/Tswipe/Components/Banner/index.jsx
@@ -5,13 +5,15 @@ import "./index.scss";
 const Banner =(props)=> { return ( <>
 
 <div className="banner-container">
-  <sup>{props.num}</sup>
-  <h2>{props.text}</h2>
-  <p>{props.info}</p>
-  <img src={img1? img1: props.img}/>
+  <sup>{props.num }</sup>
+  <h2> {props.text}</h2>
+  <p>  {props.info}</p>
+  <img src={props.img? props.img:img1}/>
   <div className="button-banner--container">
-    {props.children}
+        {props.children}
+    {/* <h5>{props.textinfo}</h5> */}
   </div>
 </div>
+{/* <p> </p> */} {/*fancy separation*/ }
 
 </> ) }; export default Banner;
--- src/pages/Tswipe/Components/TsuiteBody/index.jsx
+++ src/pages/Tswipe/Components/TsuiteBody/index.jsx
@@ -3,21 +3,30 @@ import Banner from "../Banner"
 import FancyButton from "../FancyButton";
 import "./index.scss";
 
+/* imgs */
+import res from "../Banner/ar2.png";
+import dinamicos from "../Banner/precios.png";
+import comparador from "../Banner/comparador.png";
+import pronosticos from "../Banner/pronos.png";
+
 const TsuiteBody =()=> { return ( <>
 
 <div className="tsuite-body--container">
   <div className="tsuite-banner--wrapper">
-    <Banner num="01" text="Analizador" info="de reservas">
-      <FancyButton text="Ver precios" to="" spa="5px" color="black" bg="" bor="black"/>
+    <Banner num="01" text="Analizador" info="de reservas" img={res} textinfo="insert text info here">
+      <FancyButton text="Ver analizador" to="" spa="5px" color="black" bg="" bor="black" />
     </Banner>
-    <Banner num="01" text="Analizador" info="de reservas">
-      <FancyButton text="Ver algoritmos" to="" spa="5px" color="black" bg="" bor="orange"/>
+    <Banner num="02" text="Estudio" info="de mercado">
+      <FancyButton text="estudio" to="" spa="5px" color="black" bg="" bor="orange"/>
     </Banner>
-    <Banner num="01" text="Analizador" info="de reservas">
-      <FancyButton text="Ver algoritmos" to="" spa="5px" color="black" bg="" bor="orange"/>
+    <Banner num="03" text="Precios" info="dinamicos" img={dinamicos}>
+      <FancyButton text="Ver precios" to="" spa="5px" color="black" bg="" bor="orange"/>
     </Banner>
-    <Banner num="01" text="Analizador" info="de reservas">
-      <FancyButton text="Ver algoritmos" to="" spa="5px" color="black" bg="" bor="orange"/>
+    <Banner num="04" text="Comparador" info="de canales" img={comparador}>
+      <FancyButton text="Ver comparador" to="" spa="5px" color="black" bg="" bor="var(--customcrimson)"/>
+    </Banner>
+    <Banner num="05" text="Pronosticos" info="promociones" img={pronosticos}>
+      <FancyButton text="pronosticos y promociones" to="" spa="1px" color="black" bg="" bor="lime"/>
     </Banner>
   </div>
 </div>
